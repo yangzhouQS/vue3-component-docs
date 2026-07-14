eac9d93e9797a45d565cd74c78c48c4bfb86184e
yangzhou <yearrow@qq.com>
2026-07-14 09:18:04 +0800

    fix: 关闭预览的hmr

--- rspress.config.ts
+++ rspress.config.ts
@@ -63,6 +63,9 @@ export default defineConfig({
             pluginVueJsx(),
             pluginLess(),
           ],
+          dev:{
+            hmr: false
+          }
         },
       },
     }),
